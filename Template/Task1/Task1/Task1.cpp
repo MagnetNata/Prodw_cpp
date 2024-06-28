ac1705b62ca28700c17fef837392fd9b5f9365e6
Natalya Krasnovid <natalyakrasnovid@yandex.ru>
2024-06-28 13:44:47 +0300

    Edit Template

--- Template/Task1/Task1/Task1.cpp
+++ Template/Task1/Task1/Task1.cpp
@@ -3,40 +3,49 @@
 using namespace std;
 
 template <typename T>
-T kvadrat(T a)
+T kvadrat(const T& a)
 {
 	return a * a;
 }
 
 template <typename T>
-vector <T>& kvadrat(vector <T>& v)
+vector <T>& kvadrat(const vector <T>& v)
 {
 	int kol = v.size();
+	vector <int> v1(kol);
 	for (int i = 0; i < kol; i++) 
-		v[i] = v[i] * v[i];
-	return v;
+		v1[i] = v[i] * v[i];
+	return v1;
 }
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int a;
-	vector <int> v;
-	int kol;
-	std::cout << "Введите число" << std::endl;
-	std::cin >> a;
-	std::cout << "Квадрат числа " << a << " равен " << kvadrat(a) << std::endl;
+	try {
+		setlocale(LC_ALL, "Russian");
+		int a;
+		vector <int> v;
+		vector <int> new_v;
+		int kol;
+		std::cout << "Введите число" << std::endl;
+		std::cin >> a;
+		std::cout << "Квадрат числа " << a << " равен " << kvadrat(a) << std::endl;
 
-	std::cout << "Введите количество чисел вектора" << std::endl;
-	std::cin >> kol;
-	v.resize(kol);
-	std::cout << "Введите числа вектора" << std::endl;
-	for (int i = 0; i < kol; i++)
-		std::cin >> v[i];
-	std::cout << "Квадраты чисел вектора равны " << std::endl;
-	v = kvadrat(v);
-	for (int i = 0; i < kol; i++)
-		std::cout << v[i] << " ";
-	std::cout << std::endl;
+		std::cout << "Введите количество чисел вектора" << std::endl;
+		std::cin >> kol;
+		v.resize(kol);
+		new_v.resize(kol);
+		std::cout << "Введите числа вектора" << std::endl;
+		for (int i = 0; i < kol; i++)
+			std::cin >> v[i];
+		std::cout << "Квадраты чисел вектора равны " << std::endl;
+		new_v = kvadrat(v);
+		for (int i = 0; i < kol; i++)
+			std::cout << new_v[i] << " ";
+		std::cout << std::endl;
+	}
+	catch (exception& ex)
+	{
+		std::cout << ex.what();
+	}
 	return 0;
 }
--- Template/Task2/Task2/Task2.cpp
+++ Template/Task2/Task2/Task2.cpp
@@ -2,60 +2,69 @@
 #include <vector>
 using namespace std;
 
+//Прокси для безопасного доступа к стобцам
+template <class T>
+class Proxy_ {
+public:
+	Proxy_(T* row_ptr) : row_ptr_(row_ptr) { }
+	T& operator[](size_t index) {
+		if (index < 0 || index >= sizeof(row_ptr_) / sizeof(*row_ptr_)) {
+			throw("Не корректный размер_");
+		}
+		return row_ptr_[index];
+	}
+private:
+	T* row_ptr_;
+};
+
 template <class T>
 class dw_array
 {
 private:
 	int** m;
-	int x, y;
+	size_t x_, y_;
 public:
-	dw_array(int x, int y);
-	~dw_array();
-	class Proxy {
-	public:
-		Proxy(int* _array) : _array(_array) { }
-
-		int operator[](int index) {
-			return _array[index];
+	dw_array(size_t x, size_t y): x_(x), y_(y)
+	{
+		if (x < 1 || y < 1) {
+			throw("Не корректный размер двумерного массива");
 		}
-	private:
-		int* _array;
-	};
-	Proxy operator[](int index) {
-		return Proxy(dw_array[index]);
+		m = new T * [x];
+		for (int i = 0; i < x; i++)
+		{
+			m[i] = new T[y];
+		}
+	}
+	~dw_array()
+	{
+		for (int i = 0; i < x_; i++)
+			delete[] m[i];
+		delete[] m;
+	}
+	Proxy_<T> operator[](size_t row)
+	{
+		if (row < 0 || row >= x_) {
+			throw("Не корректный размер");
+		}
+		return Proxy_<T>(m[row]);
 	}
 };
-
-template <class T>
-dw_array<T>::dw_array(int x, int y)
-{
-	this->x = x;
-	this->y = y;
-	m = new T* [x];
-	for (int i = 0; i < x; i++)
-	{
-		m[i] = new T[y];
-	}
-}
-
-template <class T>
-dw_array<T>::~dw_array()
-{
-	for (int i = 0; i < x; i++)
-		delete[] m[i];
-	delete[] m;
-}
-
-/*T& dw_array<T>::operator[] (int a)
-{
-	return dw_array(m[a]);
-}*/
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	auto test = dw_array<int>(2, 3);
-	test[0][0] = 4;
-	std::cout << test[0][0];
+	try
+	{
+		auto test = dw_array<int>(2, 3);
+		test[0][0] = 4;
+		std::cout << test[0][0];
+	}
+	catch (exception& ex)
+	{
+		std::cout << ex.what();
+	}
+	catch (const char* e) {
+		std::cout << e << std::endl;
+	}
 	return 0;
 }
