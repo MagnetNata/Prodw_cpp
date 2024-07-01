f7ce27d7950d798d7096169554596bc09ffde266
Natalya Krasnovid <natalyakrasnovid@yandex.ru>
2024-07-01 11:26:26 +0300

    Add stl1

---
+++ stl1/Task1/Task1/Task1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool sort_map(const std::pair< char, int >& a, const std::pair< char, int >& b)
+{
+    return a.second > b.second;
+}
+
+void countChar(const char* str) {
+    std::map<char, int> count;
+    int l = strlen(str);
+    for (int i = 0; i < l; i++) {
+        count[str[i]]++;
+    }
+    std::vector< std::pair< char, int > > vec(count.begin(), count.end());
+    std::sort(vec.begin(), vec.end(), sort_map);
+    for (const auto kvp : vec) {
+        std::cout << kvp.first << ": " << kvp.second << std::endl;
+    }
+}
+
+int main() 
+{
+    countChar("Hello world!!");
+    return 0;
+}
---
+++ stl1/Task2/Task2/Task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите количество чисел: " << std::endl;
+    int kol;
+    std::cin >> kol;
+    std::cout << std::endl << "Введите " << kol << " чисел: " << std::endl;
+    set<int, greater<int>> mas;
+    for (int i = 0; i < kol; i++)
+    {
+        int x;
+        std::cin >> x;
+        mas.insert(x);
+    }
+    std::cout << std::endl << "Вывод: " << std::endl;
+    for (const auto& elem : mas)
+        std::cout << elem << std::endl;
+    return 0;
+}
